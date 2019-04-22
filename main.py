4ac749fe24d9c4c7b8ffeb1060be62c032f84853
fahio12 <44535804+fahio12@users.noreply.github.com>
2019-04-22 09:48:50 -0400

    main

---
+++ main.py
@@ -0,0 +1,18 @@
+from moviepy.editor import *
+
+import os
+#create array of images and convert them into jpg!!!!!!!!!!!!!
+
+imgs = os.listdir('pics/')
+clipsArr = []
+
+for img in imgs:
+    clip = ImageClip("pics/"+img)
+    clip = clip.resize( (1920,1080) )
+    clip = clip.set_duration(t = 5)
+    clipsArr.append(clip)
+
+#change params of IMAGECLIP to the array!!!!!!!!!!!!!!!!!!!!
+print(clipsArr)
+finalClip = concatenate_videoclips(clipsArr)
+finalClip.write_videofile("movie.mp4",audio=False,fps = 24)
